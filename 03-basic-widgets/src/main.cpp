04de968b8d4b69fd9f69662dc47d7bcdb0d36b9e
Mohsen Heidari <chapanh@iran.ir>
2024-12-10 16:49:26 +0330

    Make '03-basic-widgets' Project-02

--- 03-basic-widgets/src/main.cpp
+++ 03-basic-widgets/src/main.cpp
@@ -6,11 +6,12 @@
 typedef struct {
     GtkWidget *window;
     GtkWidget *vbox;
-    GtkWidget *toggle1;
-    GtkWidget *toggle2;
+    GtkWidget *check_button1;
+    GtkWidget *check_button2;
+    GtkWidget *close_button;
 }MakeWidget;
 
-static void button_toggled(GtkToggleButton*, GtkWidget*);
+static void check_toggled(GtkToggleButton*, GtkWidget*);
 
 int main(int argc, char *argv[]) {
     MakeWidget widget;
@@ -19,20 +20,25 @@ int main(int argc, char *argv[]) {
 
     widget.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(widget.window), TITLE_NAME);
-    gtk_container_set_border_width(GTK_CONTAINER(widget.window), 20);
+    gtk_container_set_border_width(GTK_CONTAINER(widget.window), 15);
     gtk_widget_set_size_request(GTK_WIDGET(widget.window), 450, 200);
 
     g_signal_connect(G_OBJECT(widget.window), "destroy", G_CALLBACK(gtk_main_quit), widget.window);
 
-    widget.vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    widget.toggle1 = gtk_toggle_button_new_with_mnemonic("_Deactivate the other one!");
-    widget.toggle2 = gtk_toggle_button_new_with_mnemonic("_No! Deactivate that one!");
+    widget.check_button1 = gtk_check_button_new_with_label("I am the main option.");
+    widget.check_button2 = gtk_check_button_new_with_label("I rely on the other guy.");
 
-    g_signal_connect(G_OBJECT(widget.toggle1), "toggled", G_CALLBACK(button_toggled), widget.toggle2);
-    g_signal_connect(G_OBJECT(widget.toggle2), "toggled", G_CALLBACK(button_toggled), widget.toggle1);
+    /* Only enable the second check button when the first is enabled. */
+    gtk_widget_set_sensitive(GTK_WIDGET(widget.check_button2), FALSE);
+    g_signal_connect(G_OBJECT(widget.check_button1), "toggled", G_CALLBACK(check_toggled), widget.check_button2);
 
-    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.toggle1), TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.toggle2), TRUE, TRUE, 0);
+    widget.close_button = gtk_button_new_with_label("?!CLOSE THE WINDOW!?");
+    g_signal_connect(G_OBJECT(widget.close_button), "clicked", G_CALLBACK(gtk_main_quit), widget.window);
+
+    widget.vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.check_button1), TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.check_button2), TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.close_button), TRUE, TRUE, 0);
 
     gtk_container_add(GTK_CONTAINER(widget.window), GTK_WIDGET(widget.vbox));
     gtk_widget_show_all(GTK_WIDGET(widget.window));
@@ -41,11 +47,11 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-/* If the toggle button was activated, set the other as disabled. Otherwise,
-* enable the other toggle button. */
-static void button_toggled(GtkToggleButton *toggle, GtkWidget *other_toggle) {
-    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(toggle)))
-        gtk_widget_set_sensitive(GTK_WIDGET(other_toggle), FALSE);
+/* If the main check button is active, enable the other. Otherwise, disable
+* the supplementary check button. */
+static void check_toggled(GtkToggleButton *check_button1, GtkWidget *check_button2) {
+    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(check_button1)))
+        gtk_widget_set_sensitive(GTK_WIDGET(check_button2), TRUE);
     else
-        gtk_widget_set_sensitive (other_toggle, TRUE);
+        gtk_widget_set_sensitive(GTK_WIDGET(check_button2), FALSE);
 }
