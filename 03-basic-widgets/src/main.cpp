dac093bed1ce14dffa55714ad1d42d0b5fc138fb
Mohsen Heidari <chapanh@iran.ir>
2024-12-11 10:00:44 +0330

    Make '03-basic-widgets' Project-04 && RUSTY 'first-applications-rs-01' project-02

--- 03-basic-widgets/src/main.cpp
+++ 03-basic-widgets/src/main.cpp
@@ -1,4 +1,6 @@
 // Import GTK
+#include "glib.h"
+#include "glibconfig.h"
 #include <gtk/gtk.h>
 
 #define TITLE_NAME "03-basic-widgets"
@@ -6,10 +8,10 @@
 typedef struct {
     GtkWidget *window;
     GtkWidget *vbox;
-    GtkWidget *radio_button1;
-    GtkWidget *radio_button2;
-    GtkWidget *radio_button3;
-    GtkWidget *radio_button4;
+    GtkWidget *hbox;
+    GtkWidget *question;
+    GtkWidget *label;
+    GtkWidget *pass;
 }MakeWidget;
 
 int main(int argc, char *argv[]) {
@@ -20,20 +22,27 @@ int main(int argc, char *argv[]) {
     widget.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(widget.window), TITLE_NAME);
     gtk_container_set_border_width(GTK_CONTAINER(widget.window), 15);
-    gtk_widget_set_size_request(GTK_WIDGET(widget.window), 450, 400);
+    gtk_widget_set_size_request(GTK_WIDGET(widget.window), 250, 100);
 
     g_signal_connect(G_OBJECT(widget.window), "destroy", G_CALLBACK(gtk_main_quit), widget.window);
 
-    widget.vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    widget.radio_button1 = gtk_radio_button_new_with_label(nullptr, "I want to be clicked!");
-    widget.radio_button2 = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(widget.radio_button1), "Click me instead!");
-    widget.radio_button3 = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(widget.radio_button2), "NO! Click me!");
-    widget.radio_button4 = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(widget.radio_button3), "NO! Click me instead!");
+    widget.vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    widget.hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 
-    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.radio_button1), TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.radio_button2), TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.radio_button3), TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.radio_button4), TRUE, TRUE, 0);
+    gchar *str = g_strconcat("What is the password for ", g_get_user_name(), "?", NULL);
+    widget.question = gtk_label_new(str);
+    widget.label = gtk_label_new("Pasword: ");
+
+    /* Create a new GtkEntry widget and hide its content from view. */
+    widget.pass = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(widget.pass), FALSE);
+    gtk_entry_set_invisible_char(GTK_ENTRY(widget.pass), '*');
+
+    gtk_box_pack_start(GTK_BOX(widget.hbox), GTK_WIDGET(widget.label), TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(widget.hbox), GTK_WIDGET(widget.pass), TRUE, TRUE, 0);
+
+    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.question), TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(widget.vbox), GTK_WIDGET(widget.hbox), TRUE, TRUE, 0);
 
     gtk_container_add(GTK_CONTAINER(widget.window), GTK_WIDGET(widget.vbox));
     gtk_widget_show_all(GTK_WIDGET(widget.window));
@@ -41,4 +50,3 @@ int main(int argc, char *argv[]) {
     gtk_main();
     return 0;
 }
-
--- first-applications-rs-01/src/main.rs
+++ first-applications-rs-01/src/main.rs
@@ -1,42 +1,51 @@
-use gtk::{glib, Application, ApplicationWindow};
-use gtk::{prelude::*, Button};
+use std::rc::Rc;
 
-const APP_ID: &str = r#"ir.chapan_window"#;
+use glib::clone;
+use gtk::{glib, prelude::*, Application, ApplicationWindow, Box, Button};
 
-fn main() -> glib::ExitCode {
-    // Create a new application
-    let app: Application = Application::builder().application_id(APP_ID).build();
+const TITLE: &str = r#"first-applications-rs-01"#;
+const APP_ID: &str = r#"ir.app"#;
 
-    app.connect_activate(window_ui);
-
-    return app.run();
+struct MakeWidget {
+    window: Rc<ApplicationWindow>,
+    vbox: Rc<Box>,
+    button: Rc<Button>,
 }
 
-fn window_ui(app: &Application) {
-    // Create a button with label and margins
-    let button: Button = Button::builder()
-        .label("Press Me!")
-        .margin_top(50)
-        .margin_bottom(50)
-        .margin_start(50)
-        .margin_end(50)
-        .build();
-
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(|button: &Button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("You Press ME! Why?");
-    });
-
-    // Create a window and set the title
+fn ui_section(app: &Application) -> MakeWidget {
     let window: ApplicationWindow = ApplicationWindow::builder()
         .application(app)
-        .title("GTK_WITH_RUST")
-        .child(&button)
+        .title(TITLE)
         .default_width(400)
         .default_height(200)
         .build();
 
-    // Present window
+    let vbox: Box = Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .spacing(10)
+        .build();
+
+    let button: Button = Button::builder().label("EXIT BUTTON").build();
+
+    button.connect_clicked(clone!(@weak window => move |_| window.close()));
+
+    vbox.append(&button);
+    window.set_child(Some(&vbox));
+
     window.present();
+
+    return MakeWidget {
+        window: Rc::new(window),
+        vbox: Rc::new(vbox),
+        button: Rc::new(button),
+    };
+}
+
+fn main() {
+    let app: Application = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(|app| {
+        ui_section(app);
+    });
+    app.run();
 }
