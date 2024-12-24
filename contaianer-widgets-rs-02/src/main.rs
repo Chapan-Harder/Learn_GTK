1071214593ffd0f7f26159f8e2010cab41fb9745
Mohsen Heidari <chapanh@iran.ir>
2024-12-24 12:58:33 +0330

    contaianer-widgets-rs-02 project-03

--- contaianer-widgets-rs-02/src/main.rs
+++ contaianer-widgets-rs-02/src/main.rs
@@ -1,35 +1,35 @@
-use gtk::{glib, prelude::*, Application, ApplicationWindow, Box as GtkBox, Button};
+use gtk::{/*glib,*/ prelude::*, Application, ApplicationWindow, Button, Paned};
 
 const TITLE_WINDOW: &str = r#"container-widgets-rs-02"#;
 const APP_ID: &str = r#"ir.app"#;
 
-const NAMES: [&str; 4] = ["Andrew", "Joe", "Samantha", "Jonathan"];
-
 fn ui_app(app: &Application) {
-    let window = ApplicationWindow::new(app);
-    let vbox = GtkBox::new(gtk::Orientation::Vertical, 0);
-
+    let window: ApplicationWindow = ApplicationWindow::new(app);
     window.set_title(Some(TITLE_WINDOW));
-    window.set_size_request(300, 200);
+    window.set_default_size(350, 450);
 
-    for i in NAMES.iter() {
-        let button = Button::with_label(i);
-        button.set_hexpand_set(true);
-        button.set_vexpand(true);
-        vbox.append(&button);
+    let hpaned: Paned = Paned::new(gtk::Orientation::Horizontal);
+    let button1: Button = Button::with_label("peress me");
+    let button2: Button = Button::with_label("resize me");
 
-        let button_clone = button.clone();
-        button.connect_clicked(move |_| {
-            button_clone.unparent();
-        });
-    }
+    let button1_clone = button1.clone();
+    button1.connect_clicked(move |_| {
+        button1_clone.unparent();
+    });
+    let button2_clone = button2.clone();
+    button2.connect_clicked(move |_| {
+        button2_clone.unparent();
+    });
 
-    window.set_child(Some(&vbox));
+    hpaned.set_start_child(Some(&button1));
+    hpaned.set_end_child(Some(&button2));
+
+    window.set_child(Some(&hpaned));
     window.present();
 }
 
 fn main() {
-    let app = Application::builder().application_id(APP_ID).build();
+    let app: Application = Application::builder().application_id(APP_ID).build();
 
     app.connect_activate(|app| {
         ui_app(app);
